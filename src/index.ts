1f867db0a57831842d3c52b1244e34fb3b0756fa
gartot <925252016@qq.com>
2023-06-08 23:35:13 +0800

    fix: 拼写调整

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { deallink } from './utils/addr';
-import { getVideoDownLinkByurl } from './utils/download';
-import { UserAgent, ArticulationEnum } from './constant';
-import type { Option, RequestHeaderType } from './types/types';
+import {deallink} from './utils/addr';
+import {getVideoDownLinkByurl} from './utils/download';
+import {ArticulationEnum, UserAgent} from './constant';
+import type {Option, RequestHeaderType} from './types/types';
 
 /**
  * 下载哔哩哔哩的视频
@@ -14,20 +14,19 @@ const downBili = async (opt: Option) => {
     throw new Error('必须传入url!!');
   }
 
-  const requeseHeader: RequestHeaderType = {
+  const requestHeader: RequestHeaderType = {
     Referer: url,
     Cookie: '',
     'User-Agent': UserAgent,
   };
 
   if (opt.sessdata) {
-    requeseHeader['Cookie'] = 'SESSDATA=' + opt.sessdata;
+    requestHeader['Cookie'] = 'SESSDATA=' + opt.sessdata;
     level = ArticulationEnum._1080PLUS;
     opt.level = opt.level ?? ArticulationEnum._1080PLUS; // 有会员获取的视频自动设置为1080p+
   }
   const addr = await getVideoDownLinkByurl(url, level);
-  const dealResult = await deallink(opt, requeseHeader, addr, url);
-  return dealResult;
+  return await deallink(opt, requestHeader, addr, url);
 };
 
 export { downBili };
