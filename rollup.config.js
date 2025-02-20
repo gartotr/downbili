d329dc88eb3319ba7ca7c15ccde2669b42deb87d
gartotr <h925252016@gmail.com>
2025-02-20 22:22:59 +0800

    feat: rollup打包

---
+++ rollup.config.js
@@ -0,0 +1,52 @@
+import typescript from 'rollup-plugin-typescript2';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
+
+const commonConfig = {
+  input: 'src/index.ts', // 修改为你的源码入口路径
+  external: ['axios', 'fluent-ffmpeg', 'single-line-log', 'path', 'fs', 'child_process'],
+  plugins: [
+    resolve({
+      preferBuiltins: true,
+    }),
+    commonjs(),
+    typescript({
+      useTsconfigDeclarationDir: true,
+      tsconfigOverride: {
+        compilerOptions: {
+          declarationDir: 'dist/types',
+        },
+      },
+    }),
+  ],
+};
+
+export default [
+  {
+    ...commonConfig,
+    output: {
+      dir: 'dist/cjs',
+      format: 'cjs',
+      exports: 'auto',
+    },
+  },
+  {
+    ...commonConfig,
+    output: {
+      dir: 'dist/esm',
+      format: 'esm',
+      preserveModules: true,
+    },
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [
+      {
+        file: 'dist/types/index.d.ts',
+        format: 'es',
+      },
+    ],
+    plugins: [dts()],
+  },
+];
